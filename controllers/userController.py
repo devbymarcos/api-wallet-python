c8af457d0e52a3d7314aa04f9b7b785233afd3a1
Marcos <marcoslopes.dev@gmasil.com>
2023-09-19 09:29:01 -0300

    feat: agora temos a pasta controllers para organizar as ações

--- controllers/userController.py
+++ controllers/userController.py
@@ -1,6 +1,41 @@
 
-from database import get_session, User
+from flask import Blueprint, jsonify, Response
+from models.users import Users
+import json
 
-def get_all_users():
-    session = get_session()
-    users = session.query(User).all()
+
+
+
+def find_user(id):
+    user = Users.query.get(id)
+
+    if user: 
+        user_data={
+            "id":user.id,
+            "first_name":user.first_name,
+            "last_name" : user.last_name,
+            "email": user.email,
+            "photo": user.photo
+        }
+
+        return jsonify(user_data)
+    else:
+        error_message = {"message": "usuário não encontrado" }
+        response = Response(json.dumps(error_message),status=404,mimetype='application/json')
+        return response
+    
+def find_users():
+    users = Users.query.all()
+    user_list = []
+    for user in users:
+        user_data = {
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "photo": user.photo
+        }
+        user_list.append(user_data)
+    return jsonify(user_list)
+
+
--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,41 +1,19 @@
-from flask import Blueprint, jsonify, Response
-from models.users import Users
-import json
+from flask import Blueprint 
+
+
+from controllers.userController import find_users, find_user 
 
 
 user_bp = Blueprint('user',__name__)
 
 @user_bp.get("/users")
-def find_users():     
-    users = Users.query.all()
-    user_list =[]
-    for user in users:
-        user_data={
-            "id":user.id,
-            "first_name":user.first_name,
-            "last_name" : user.last_name,
-            "email": user.email,
-            "photo": user.photo
-        }
-        user_list.append(user_data)
-    return jsonify(user_list)
+def route_users():
+    result = find_users()
+    return result
+
 
 @user_bp.get("/user/<int:id>")
-def  find_user(id):
-    user = Users.query.get(id)
-
-    if user: 
-        user_data={
-            "id":user.id,
-            "first_name":user.first_name,
-            "last_name" : user.last_name,
-            "email": user.email,
-            "photo": user.photo
-        }
-
-        return jsonify(user_data)
-    else:
-        error_message = {"message": "usuário não encontrado" }
-        response = Response(json.dumps(error_message),status=404,mimetype='application/json')
-        return response
+def route_user(id):
+    result = find_user(id)
+    return result
     
