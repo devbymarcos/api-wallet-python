09bd887b7fd5ac247c21d5474ad727197e2c3152
Marcos <marcoslopes.dev@gmail.com>
2023-10-27 11:48:46 -0300

    feat: requirements.txt

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-## Api wallet-control
 
-Este é um projeto de estudo e está em desenvolvimento.
-
-
-## Objetivo
-
-Compreender o uso do Python com Flask
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -2,23 +2,23 @@ from flask import Blueprint, jsonify, Response, request
 from models.users import Users
 
 
-auth_bp = Blueprint('auth',__name__)
+auth_bp = Blueprint('auth', __name__)
+
 
 @auth_bp.post("/login")
 def auht_user():
-    
-    #validar aqui
+    # validar aqui
     email = request.form.get("email")
     user = Users.query.filter_by(email=email).first()
 
-    if user : 
-        user_data={
-            "id":user.id,
-            "first_name":user.first_name,
-            "last_name" : user.last_name,
+    if user:
+        user_data = {
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
             "email": user.email,
             "photo": user.photo,
-            "password":user.password
+            "password": user.password
         }
     else:
-        return 
+        return
